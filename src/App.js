f4d95dff83f26016e7d45ece5f7ece6d112f88a9
nandu44-code <nanduganeshnair4444@gmail.com>
2023-12-23 09:35:22 +0530

    imported BrowserRouter from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route ,Routes} from 'react-router-dom';
 import Navbar from './components/NavBar';
 
 function App() {
   return (
     <>
     <Navbar />
+    <Routes>
+    <Route path="/" element={<Home />} />
+    </Routes>
     </>
     
 
